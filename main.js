826cf1aaa00af7086a73a706ccf251430a91a28b
Lrigami <prune.thiry@hotmail.com>
2025-05-28 17:19:11 +0200

    refactor: remove empty space

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const path = require('path');
 const fs = require('fs');
 const { autoUpdater } = require('electron-updater');
 
-
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 950,
